b9c228d2e58d90cc69f4e779e0fd7d84358d323e
Abdelrahman Sadat <abdelrahman.sadat@ithraa.sa>
2024-03-10 20:14:06 +0200

    - chore: added zod and created validation middleware

---
+++ API/middleware/validate.ts
@@ -0,0 +1,22 @@
+import { NextFunction, Request, Response } from "express";
+import { z } from "zod";
+
+export const validate = (
+  schema: z.ZodSchema<any>,
+  location: "body" | "query"
+) => {
+  return (req: Request, res: Response, next: NextFunction) => {
+    const data = location === "body" ? req.body : req.query;
+    const result = schema.safeParse(data);
+
+    if (!result.success) {
+      res.status(400).send({
+        error: true,
+        message: result.error.issues.map((issue) => issue.message).join(", "),
+      });
+    } else {
+      //   Object.assign(location === "body" ? req.body : req.query, result.data);
+      next();
+    }
+  };
+};
