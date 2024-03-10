4fd609865e88f47989c8898df864ce4f8edc3363
Abdelrahman Sadat <abdelrahman.sadat@ithraa.sa>
2024-03-10 21:17:09 +0200

    - chore/fix: added bodyParser - I'm still surprised how this is not built into express yet...

--- API/src/app.ts
+++ API/src/app.ts
@@ -2,10 +2,14 @@ import express from "express";
 import dotenv from "dotenv";
 
 import { userRouter } from "./routes/userRouter";
+import bodyParser from "body-parser";
 
 dotenv.config();
 
 const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
